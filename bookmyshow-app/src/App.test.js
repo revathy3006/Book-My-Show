509be43b10165a80f0d64e53dc9f0327688813a9
revathy3006 <revathyjawahar@gmail.com>
2026-08-27 00:43:16 +0530

    Refactor App.test.js to test BookMyShow application
    Updated test to render the App component within Provider and BrowserRouter.

--- bookmyshow-app/src/App.test.js
+++ bookmyshow-app/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './Redux/store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders BookMyShow application', () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
 });
