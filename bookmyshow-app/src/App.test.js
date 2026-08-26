e9ffe3b1c3ca640a75e42ba4d78aeecfeb9f0c29
revathy3006 <revathyjawahar@gmail.com>
2026-08-27 00:52:01 +0530

    Update App.test.js

--- bookmyshow-app/src/App.test.js
+++ bookmyshow-app/src/App.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import store from './Redux/store';
+import { store } from './Redux/store';
 
 test('renders BookMyShow application', () => {
   render(
